cec172db2d40e2d35064a39a2f83899c655550a7
Andy <andyau.aa@gmail.com>
2018-01-28 17:37:09 -0800

    initial commit with existing api for user authentication. Includes API end points for user registration, logging in and updating user attributes.

---
+++ conduit/apps/authentication/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from .views import (
+    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
+)
+
+
+urlpatterns = [
+    url(r'^users/?$', UserRetrieveUpdateAPIView.as_view()),
+    url(r'^users/?$', RegistrationAPIView.as_view()),
+    url(r'^users/login/?$', LoginAPIView.as_view()),
+]
--- conduit/settings.py
+++ conduit/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'conduit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'conduit',
+        'USER': 'andy',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -127,3 +130,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Tell Django about the custom `User` model we created. The string
+# `authentication.User` tells Django we are referring to the `User` model in
+# the `authentication` module. This module is registered above in a setting
+# called `INSTALLED_APPS`.
+AUTH_USER_MODEL = 'authentication.User'
+
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'conduit.apps.core.exceptions.core_exception_handler',
+    'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'conduit.apps.authentication.backends.JWTAuthentication',
+    ),
+}
